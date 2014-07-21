5fe5a0d4993178b3912edf994cd559d755bc1909
MiracleBlue <foreverskybound@gmail.com>
2014-07-21 22:06:10 +1000

    Initial commit, standard structure

---
+++ src/animations/bounce.js
@@ -0,0 +1,9 @@
+define(function(require) {
+	return function() {
+		// Bouncy!
+		// The way to access this animation system is to get the modifier like so
+		// var bounceIn = Tricks.bounceIn();
+		// myViewObject.add(bounceIn.modifier).add(mySurface);
+		// bounceIn.full();
+	}
+});
---
+++ src/tricks.js
@@ -0,0 +1,7 @@
+/**
+ * Tricks.js is a pre-fabricated, just-add-water animations library for famo.us.
+ *
+ * Lovingly crafted by Nicholas Kircher, provided to the world under the MIT license.
+ *
+ * #version: 0.1
+ **/
